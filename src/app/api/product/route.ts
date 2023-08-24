2c534dfa1a46b7d92c7095a28d084a436a68da2e
Mwero Abdalla <mwerothewebmaker@gmail.com>
2023-08-24 17:53:08 +0300

    chore : add dashboard

--- src/app/api/product/route.ts
+++ src/app/api/product/route.ts
@@ -1,21 +1,23 @@
-import { createRecord, db, getRecords } from '@/libs';
+import { products } from '@/dummy_data/products';
+import { createRecord, getRecords } from '@/libs/mongoose/mongoseCrud';
 import { NextResponse } from 'next/server';
 const table = "products"
 
 export async function GET(request: Request) {
- 
- 
+
+
   try {
+
     const data = await getRecords(table);
-    
-    return new Response(JSON.stringify(data||[]), {
+
+    return new Response(JSON.stringify(Object.values(data).length > 0 ? data : products), {
       status: 200,
       statusText: 'OK',
     });
   } catch (error: any) {
     return new Response(JSON.stringify({ error: error.message }), {
       status: 500
-          });
+    });
   }
 }
 
@@ -24,12 +26,12 @@ export async function POST(request: Request) {
     let body = await request.json();
 
     const result = await createRecord(table, body);
-   
-   
-    return NextResponse.json({result},{status:201});
+
+
+    return NextResponse.json({ result }, { status: 201 });
   } catch (error: any) {
     console.log({ error: error.message });
-    return NextResponse.json({ error: error.message }, {status:500});
-    
+    return NextResponse.json({ error: error.message }, { status: 500 });
+
   }
 }

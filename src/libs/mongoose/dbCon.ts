63ffbd64c6fa213fda556534cb835bbfd7e3ca9f
Mwero Abdalla <mwerothewebmaker@gmail.com>
2023-08-29 04:39:16 +0300

    fix : bug-fix in mongose

--- src/libs/mongoose/dbCon.ts
+++ src/libs/mongoose/dbCon.ts
@@ -3,18 +3,20 @@ import mongoose from 'mongoose';
 // c9XddCqLzuNiDNDQ
 
 function getDbURI(dbname: string) {
-  const MONGO_DB_URI_DEV = `mongodb://localhost:27017/${dbname}?retryWrites=true&w=majority`;
+  const MONGO_DB_URI_DEV = `mongodb://127.0.0.1:27017/${dbname}?retryWrites=true&w=majority`;
   const ENV = process.env.NODE_ENV || 'developemnt'
 
   return ENV === "production"
-    ? `mongodb+srv://${process.env.NEXT_PUBLIC_MONGO_USER}:${process.env.MONGO_PWD}@cluster0.2f29nts.mongodb.net/${dbname}?retryWrites=true&w=majority`
+    ? `mongodb+srv://${process.env.NEXT_PUBLIC_MONGO_USER}:${process.env.NEXT_PUBLIC_MONGO_PWD}@cluster0.2f29nts.mongodb.net/${dbname}?retryWrites=true&w=majority`
     : MONGO_DB_URI_DEV;
 
 }
-const MONGO_DB_URI = getDbURI('splendid_media_db')
+
 
 // export const db = mongoose.createConnection(MONGO_DB_URI);
 export async function dbCon() {
+
+  const MONGO_DB_URI = getDbURI('splendid_media_db')
   // Connecting to MongoDB using Mongoose
   mongoose.connect(MONGO_DB_URI);
   // mongoose.connect('mongodb://127.0.0.1:27017/mydatabase');
@@ -24,25 +26,10 @@ export async function dbCon() {
     console.error('MongoDB connection error:', error);
     return false
   });
-  
+
   db.once('open', () => {
     console.log('Connected to MongoDB database.');
     return db;
   });
 
 }
-
-// export async function dbClose() {
-//   // Connecting to MongoDB using Mongoose
-//   // mongoose.connect('mongodb://127.0.0.1:27017/mydatabase');
-//   // const db = mongoose.connection;
-
-//   // db.on('error', (error) => {
-//   //   console.error('MongoDB connection error:', error);
-//   // });
-//   // db.once('open', () => {
-//   //   console.log('Connected to MongoDB database.');
-//   // });
-//   await dbCon().close()
-
-// }

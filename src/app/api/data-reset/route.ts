a48221d430270601cbb71af6e38994fd5623c30a
Mwero Abdalla <mwerothewebmaker@gmail.com>
2023-09-22 18:24:07 +0300

    chore : add password-reset mechanism

---
+++ src/app/api/data-reset/route.ts
@@ -0,0 +1,27 @@
+import { deleteAll } from "@/libs/mongoose/mongoseCrud";
+import { getSearchParams } from "@/utils/key_functions";
+
+
+export async function GET(request: Request) {
+  const table = getSearchParams(request.url,"table");
+
+  try {
+    
+    if (table) {
+     await deleteAll(table);
+   
+    }
+
+    return new Response(JSON.stringify({msg:"Deleted successfully"}), {
+      status: 200,
+      statusText: "OK",
+    });
+  } catch (error: any) {
+    console.log({ error: error.message });
+    return new Response(JSON.stringify({ error: error.message }), {
+      status: 500,
+    });
+  }
+}
+
+  

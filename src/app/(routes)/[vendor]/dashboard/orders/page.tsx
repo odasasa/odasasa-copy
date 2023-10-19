67d2bbe13d91dced8aa6736e85d529db16314099
Mwero Abdalla <mwerothewebmaker@gmail.com>
2023-10-19 20:16:02 +0300

    chore : add search params in the dashboard

--- src/app/(routes)/[vendor]/dashboard/orders/page.tsx
+++ src/app/(routes)/[vendor]/dashboard/orders/page.tsx
@@ -6,9 +6,9 @@ import { useSearchParams } from "next/navigation";
 import { FaEdit } from "react-icons/fa";
 
 export default function OrderPage({ params }: any) {
-  const searchParams = useSearchParams()
+  const searchParams = useSearchParams();
   const { data: orders, error } = useFetch(
-    `/api/orders?vendor=${searchParams.get('owner')||params.vendor}`
+    `/api/orders?vendor=${searchParams.get("owner") || params.vendor}`
   );
   if (!Array.isArray(orders))
     return (
@@ -16,27 +16,27 @@ export default function OrderPage({ params }: any) {
     );
   return (
     <>
-      {orders.map((p: any, indx: number) => (
+      {orders.map((currentOrder: any, indx: number) => (
         <div
-          key={`${p._id}-${indx}`}
+          key={`${currentOrder._id}-${indx}`}
           className="w-full overflow-x-hidden grid grid-cols-4 border-b-2 border-solid hover:bg-[#f9f9ff] py-3 mx-1 text-sm"
         >
           <span className="overflow-hidden">{indx + 1}</span>
 
-          <span className="overflow-hidden">{p.name}</span>
+          <span className="overflow-hidden">{currentOrder.name}</span>
 
-          {/* <span className="overflow-hidden">{p.units}</span> */}
+          {/* <span className="overflow-hidden">{currentOrder.units}</span> */}
 
           <span className={`overflow-hidden flex justify-center items-center`}>
             {" "}
             <span
               className={`p-1 rounded-full mx-1 ${
-                p.status.toLowerCase().includes("active")
+                currentOrder.status.toLowerCase().includes("active")
                   ? "bg-green-300"
                   : "bg-red-400"
               }`}
             ></span>
-            {" " + strCapitalize(p.status)}
+            {" " + strCapitalize(currentOrder.status)}
           </span>
           <div className="flex justify-between items-center ">
             <button
@@ -48,7 +48,7 @@ export default function OrderPage({ params }: any) {
               {" "}
               <FaEdit className="text-lg text-orange-400" />
             </button>
-            <DeleteButton id={p._id!} table="category" />
+            <DeleteButton id={currentOrder._id!} table="category" />
           </div>
         </div>
       ))}

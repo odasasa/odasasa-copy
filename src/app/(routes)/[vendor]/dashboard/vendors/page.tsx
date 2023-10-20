8c0da49d41f5071365c9f08fa70ca612ea687666
Mwero Abdalla <mwerothewebmaker@gmail.com>
2023-10-20 13:25:42 +0300

    fix : bug-fix in vendors layout

--- src/app/(routes)/[vendor]/dashboard/vendors/page.tsx
+++ src/app/(routes)/[vendor]/dashboard/vendors/page.tsx
@@ -34,22 +34,24 @@ export default function VendorsPage({ params }: any) {
     );
   return (
     <>
-    <Typography variant="h2" className="w-full underline">Vendors Page</Typography>
-      <div className="w-full overflow-x-hidden grid grid-cols-6 mid:grid-cols-6 border-b-2 border-solid hover:bg-[#f9f9ff] py-3 mx-1 text-sm font-bold bg-[#f9f9ff] ">
+      <Typography variant="h2" className="w-full underline">
+        Vendors Page
+      </Typography>
+      <div className="w-full overflow-x-hidden grid grid-cols-3 md:grid-cols-6 border-b-2 border-solid hover:bg-[#f9f9ff] py-3 mx-1 text-sm font-bold bg-[#f9f9ff] ">
         <span className="p-3">#</span>
         <span className="hidden md:flex">Business Name</span>
         <span>Business Code</span>
-        <span className="text-center">Status</span>
-        <span>Email</span>
+        <span className="text-center hidden md:flex">Status</span>
+        <span className="hidden md:flex">Email</span>
         <span>Phone</span>
-        <span className="hidden">Operation</span>
+      
       </div>
       {vendors.map((currentVendor: any, indx: number) => (
         <Link
           // href={`/${currentVendor.vendor}/dashboard?owner=${params.vendor}`}
           href={`/${params.vendor}/dashboard?owner=${currentVendor.vendor}`}
           key={`${currentVendor._id}-${indx}`}
-          className="w-full overflow-x-hidden grid grid-cols-6 mid:grid-cols-6 border-b-2 border-solid hover:bg-[#f9f9ff] py-3 mx-1 text-sm"
+          className="w-full overflow-x-hidden grid grid-cols-3 md:grid-cols-6 border-b-2 border-solid hover:bg-[#f9f9ff] py-3 mx-1 text-sm"
         >
           <span className="overflow-hidden p-3">{indx + 1}</span>
 
@@ -59,7 +61,7 @@ export default function VendorsPage({ params }: any) {
 
           <span className="overflow-hidden ">{currentVendor.vendor}</span>
 
-          <span className={`overflow-hidden flex justify-center items-center`}>
+          <span className={`overflow-hidden hidden md:flex justify-center items-center`}>
             {" "}
             <span
               className={`p-1 rounded-full mx-2 ${
@@ -68,20 +70,9 @@ export default function VendorsPage({ params }: any) {
             ></span>
             {" " + strCapitalize(currentVendor.status ? "Active" : "Inactive")}
           </span>
-          <span>{currentVendor.email}</span>
+          <span className="hidden md:flex">{currentVendor.email}</span>
           <span>{currentVendor.phone}</span>
-          <div className=" justify-between items-center hidden">
-            <button
-              className="border-2 border-solid border-orange-400 p-2 rounded-md"
-              onClick={() => {
-                // setData({ ...globalData, isModalOpen: !isModalOpen });
-              }}
-            >
-              {" "}
-              <FaEdit className="text-lg text-orange-400" />
-            </button>
-            <DeleteButton id={currentVendor._id!} table="category" />
-          </div>
+          
         </Link>
       ))}
     </>
